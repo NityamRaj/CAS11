0213844fada3daed1a3aef4ca7c2e901336972a2
NityamRaj <90399024+NityamRaj@users.noreply.github.com>
2023-10-20 01:26:34 +0530

    fourth commit

--- js/main(1).js
+++ js/main(1).js
@@ -1,48 +1,47 @@
 const responsive = {
-    0: {
-        items: 1
-    },
-    320: {
-        items: 1
-    },
-    560: {
-        items: 2
-    },
-    960: {
-        items: 3
-    }
-}
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  },
+};
 
 $(document).ready(function () {
+  $nav = $('.nav');
+  $toggleCollapse = $('.toggle-collapse');
 
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
+  $toggleCollapse.click(function () {
+    $nav.toggleClass('collapse');
+  });
 
-    /** click event on toggle menu */
-    $toggleCollapse.click(function () {
-        $nav.toggleClass('collapse');
-    })
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [
+      $('.owl-navigation .owl-nav-prev'),
+      $('.owl-navigation .owl-nav-next'),
+    ],
+    responsive: responsive,
+  });
 
-    // owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
+  $('.move-up span').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 
-
-    // click to scroll top
-    $('.move-up span').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    })
-
-    // AOS Instance
-    AOS.init();
-
+  AOS.init();
 });
